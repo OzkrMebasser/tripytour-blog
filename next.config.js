8782f4a79b6ca8da582cf5757cbdb961ed5a52d4
OzkrMebasser <oscarmorenoestudios@gmail.com>
2024-02-15 23:39:28 -0800

    Feat: adding config for deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-      domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
-    }
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*', // Proxy to Backend
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
