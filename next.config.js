11ad1a0826582d37ad5b144d2031412112c4dc9e
OzkrMebasser <oscarmorenoestudios@gmail.com>
2024-02-29 21:47:56 -0800

    Feat: deploy config

--- next.config.js
+++ next.config.js
@@ -4,27 +4,27 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
   },
-  async redirects() {
-    return [
-      {
-        source: '/home',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
-  async rewrites() {
-    return [
-      // {
-      //   source: '/api/:path',
-      //   destination: 'http://localhost:3000/api/:path', // Proxy to Backend
-      // }
-      {
-        source: '/api/:path',
-        destination: 'https://tripytour-blog.vercel.app/api/:path',
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/home',
+  //       destination: '/',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
+  // async rewrites() {
+  //   return [
+  //     // {
+  //     //   source: '/api/:path',
+  //     //   destination: 'http://localhost:3000/api/:path', // Proxy to Backend
+  //     // }
+  //     {
+  //       source: '/api/:path',
+  //       destination: 'https://tripytour-blog.vercel.app/api/:path',
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
